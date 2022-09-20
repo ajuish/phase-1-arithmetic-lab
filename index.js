0a9a817ada1d5ed774e0704d0a5e07af013db2a1
Alex Ju <aju217@gmail.com>
2022-09-19 23:34:11 -0400

    commit tree 6427595a64022f80356122899419b83372c65aad

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 20
+let num4 = 8
+
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random()*100) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(num2,num3,num4);
